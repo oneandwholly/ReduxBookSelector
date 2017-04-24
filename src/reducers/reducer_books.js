0f9b9239d628b2ca10fea9b40ad080784a2f6035
Jon Huh <oneandwholly@gmail.com>
2017-04-24 19:25:21 +0900

    Make BookReducer and hook it up to the state using combineReducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function () {
+  return [
+    { title: 'Javascript: The Good Parts'},
+    { title: 'Harry Potter'},
+    { title: 'Lord of The Rings'},
+    { title: 'Eloquent Javascript'}
+  ];
+}
