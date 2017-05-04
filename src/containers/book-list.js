6d7d32cb591892e6402aee0036bf7e4ca4732b58
Jon Huh <oneandwholly@gmail.com>
2017-05-04 18:14:00 +0900

    Create selectBook action creator and bind it to the props of BookList container

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { selectBook } from '../actions/index';
+import { bindActionCreators } from 'redux';
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map(book => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          key={book.title}
+          className="list-group-item"
+          onClick={() => this.props.selectBook(book)} >
+          {book.title}
+        </li>
       );
     });
   }
@@ -27,4 +34,11 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+function mapDispatchToProps(dispatch) {
+  // whenever selectBook is called, the result should be passed
+  // to all of our reducers
+
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
