82166ad190d17755813690c3d60628a25168315d
Jon Huh <oneandwholly@gmail.com>
2017-05-04 18:48:51 +0900

    Create BookDetail container and map books state to render book details

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function () {
   return [
-    { title: 'Javascript: The Good Parts'},
-    { title: 'Harry Potter'},
-    { title: 'Lord of The Rings'},
-    { title: 'Eloquent Javascript'}
+    { title: 'Javascript: The Good Parts', pages: 101},
+    { title: 'Harry Potter', pages: 505},
+    { title: 'Lord of The Rings', pages: 1043},
+    { title: 'Eloquent Javascript', pages: 42}
   ];
 }
